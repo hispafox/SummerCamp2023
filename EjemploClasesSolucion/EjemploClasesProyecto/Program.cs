f7e6d9ef99db5811a77b7c27d5930d2fc5391ec2
hispafox <hispafox@gmail.com>
2023-07-18 12:42:02 +0200

    Commit inicial

---
+++ EjemploClasesSolucion/EjemploClasesProyecto/Program.cs
@@ -0,0 +1,56 @@
+﻿using Entidades;
+
+namespace EjemploClasesProyecto
+{
+    // Calcular salario de empleados
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Crear y asignar propiedades mediante la instancia
+            var jose = new Empleado();
+            jose.Nombre = "Jose";
+            jose.FechaAlta = new DateTime(2021, 01, 10);
+            jose.Salario = 20000;
+            jose.Alta = true;
+            jose.FijarNivel(Empleado.Nivel.excelente);
+
+
+            // Crear y asignar valores a propiedades directamente
+            var maria = new Empleado()
+            {
+                Nombre = "Maria",
+                FechaAlta = new DateTime(2022, 03, 01),
+                Salario = 20000,
+                Alta = true
+            };
+            maria.FijarNivel(Empleado.Nivel.excelente);
+
+            // Crear instancia y pasarle parametros al constructor
+            var juan = new Empleado("Juan",
+                                    20000,
+                                    new DateTime(2023,01,15),
+                                    true);
+            juan.FijarNivel(Empleado.Nivel.excelente);
+
+            var numeros = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8 };
+            numeros.Add(9);
+
+
+            var lista = new List<Empleado>();
+            lista.Add(maria);
+            lista.Add(jose);
+            lista.Add(juan);
+
+            foreach (var empleado in lista) {
+
+                empleado.CalcularAumentoSalario();
+            
+            }
+
+
+
+        }
+    }
+}
---
+++ EjemploClasesSolucion/Entidades/Empleado.cs
@@ -0,0 +1,53 @@
+﻿namespace Entidades
+{
+    // https://learn.microsoft.com/es-es/dotnet/csharp/programming-guide/classes-and-structs/access-modifiers
+    public class Empleado
+    {
+
+        public enum Nivel
+        {
+            normal,
+            bueno,
+            excelente
+
+        }
+
+           private Nivel _nivel;
+
+        public void FijarNivel(Nivel nivel)
+        {
+            this._nivel = nivel;
+        }
+
+
+        public Empleado()
+        {
+
+        }
+
+        public Empleado(string nombre,
+                         double salario,
+                         DateTime fechaAlta,
+                         bool alta)
+        {
+            Nombre = nombre;
+            Salario = salario;
+            FechaAlta = fechaAlta;
+            Alta = alta;
+        }
+
+
+        public string Nombre { get; set; }
+
+        public double Salario { get; set; }
+
+        public DateTime FechaAlta { get; set; } = DateTime.Now;
+
+        public bool Alta { get; set; }
+
+        public void CalcularAumentoSalario()
+        {
+            //throw new NotImplementedException();
+        }
+    }
+}
